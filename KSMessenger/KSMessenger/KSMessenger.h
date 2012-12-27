99c817a556acbe64b1a676122b61639e731b2b32
sassembla <toru.inoue@kissaki.tv>
2012-12-27 18:34:24 +0900

    add original resources

---
+++ KSMessenger/KSMessenger/KSMessenger.h
@@ -0,0 +1,13 @@
+//
+//  KSMessenger.h
+//  KSMessenger
+//
+//  Created by sassembla on 2012/12/27.
+//  Copyright (c) 2012年 KISSAKI Inc,. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KSMessenger : NSObject
+
+@end
---
+++ KSMessenger/KSMessenger/KSMessenger.m
@@ -0,0 +1,13 @@
+//
+//  KSMessenger.m
+//  KSMessenger
+//
+//  Created by sassembla on 2012/12/27.
+//  Copyright (c) 2012年 KISSAKI Inc,. All rights reserved.
+//
+
+#import "KSMessenger.h"
+
+@implementation KSMessenger
+
+@end
