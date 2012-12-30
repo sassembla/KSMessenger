db5989f59c97877d96114b66c3fe9d58efb65c28
sassembla <toru.inoue@kissaki.tv>
2012-12-30 11:42:25 +0900

    add monitor

---
+++ KSMessengerMonitorViewControl/KSMessengerMonitorViewController.h
@@ -0,0 +1,13 @@
+//
+//  KSMessengerMonitorViewController.h
+//  KSMessenger
+//
+//  Created by sassembla on 2012/12/30.
+//  Copyright (c) 2012年 KISSAKI Inc,. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSMessengerMonitorViewController : UIViewController
+
+@end
---
+++ KSMessengerMonitorViewControl/KSMessengerMonitorViewController.m
@@ -0,0 +1,38 @@
+//
+//  KSMessengerMonitorViewController.m
+//  KSMessenger
+//
+//  Created by sassembla on 2012/12/30.
+//  Copyright (c) 2012年 KISSAKI Inc,. All rights reserved.
+//
+
+#import "KSMessengerMonitorViewController.h"
+
+@interface KSMessengerMonitorViewController ()
+
+@end
+
+@implementation KSMessengerMonitorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
